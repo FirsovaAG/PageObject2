8304cdc2cb0c03b66b7ff8e4bbf528417683c4a1
FirsovaAG <61489394+FirsovaAG@users.noreply.github.com>
2020-04-05 20:17:19 +0300

    check login page (url, login forn, register form)

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,5 @@
 from .base_page import BasePage
+from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
@@ -8,13 +9,15 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
-        assert True
+        assert self.url.find("login") > 0, "Link of login_page is not corrected"
+        # реализация проверки url
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
-        assert True
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not found"
+        #проверка, что есть форма логина
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
-        assert True
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not found"
+        #проверка, что есть форма регистрации на странице
+
+        
