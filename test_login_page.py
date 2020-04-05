951159745193aaa50283a95521e7178873268c29
FirsovaAG <61489394+FirsovaAG@users.noreply.github.com>
2020-04-05 20:23:26 +0300

    check lofin page added in test_login_page.py

---
+++ test_login_page.py
@@ -0,0 +1,9 @@
+from .pages.login_page import LoginPage
+import time
+
+def test_guest_should_see_login_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    time.sleep(10)
+    page.should_be_login_page()
--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,4 @@
 from .pages.main_page import MainPage
-from .pages.login_page import LoginPage
 import time
 
 
@@ -15,10 +14,4 @@ def test_guest_should_see_login_link(browser):
     page.open()
     page.should_be_login_link()
 
-def test_guest_should_see_login_form(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page = LoginPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-    page.open()                      # открываем страницу
-    time.sleep(10)
-    page.should_be_login_page()
 
