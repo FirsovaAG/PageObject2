cb5e34eadec9eb0ff1877521e5212c581f90b07f
FirsovaAG <61489394+FirsovaAG@users.noreply.github.com>
2020-04-05 19:15:43 +0300

    added message for mistake NoSuchElementException

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,12 +1,20 @@
-
+from selenium.common.exceptions import NoSuchElementException
 
 class BasePage():
 
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         #Внутри конструктора сохраняем эти данные как аттрибуты нашего класса
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         #добавим еще один метод, который открывает нужную страницу в браузере
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
