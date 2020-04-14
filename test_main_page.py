e5871148af57e20f5d11fb82557aa0b5ffd0f7e9
FirsovaAG <61489394+FirsovaAG@users.noreply.github.com>
2020-04-14 20:46:52 +0300

    added class TestLoginFromMainPage()

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,12 @@
-from .pages.main_page import MainPage
+from .pages.base_page import BasePage
 from .pages.login_page import LoginPage
+import pytest
+
 import time
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com"
-    page = MainPage(browser, link)
+    page = BasePage(browser, link)
     page.open()
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
@@ -13,7 +15,37 @@ def test_guest_can_go_to_login_page(browser):
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
+    page = BasePage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = BasePage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    page.should_not_be_product_in_a_basket()
+
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    # не забываем передать первым аргументом self                       
+    def test_guest_can_go_to_login_page(self, browser):     
+        link = "http://selenium1py.pythonanywhere.com"
+        page = BasePage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
+        time.sleep(5)
+
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = BasePage(browser, link)
+        page.open()
+        page.should_be_login_link()
+
+
+
+    
+
 
