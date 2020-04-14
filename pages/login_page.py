21a023c54e56d6907e14887e36beac36f05c6dd8
FirsovaAG <61489394+FirsovaAG@users.noreply.github.com>
2020-04-14 20:50:35 +0300

    added check to login in website

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,6 @@
 from .base_page import BasePage
 from .locators import LoginPageLocators
+import time
 
 
 class LoginPage(BasePage):
@@ -20,4 +21,39 @@ class LoginPage(BasePage):
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not found"
         #проверка, что есть форма регистрации на странице
 
+    def register_for_guest(self):
+        input_registre_email = self.browser.find_element(*LoginPageLocators.REGISTER_EMAIL)
+        input_registre_email.send_keys(f"{time.time()}@gmail.com")
+        input_registre_password1 = self.browser.find_element(*LoginPageLocators.REGISTER_PASSWORD1)
+        input_registre_password1.send_keys("sonfgys19")
+        input_registre_password2 = self.browser.find_element(*LoginPageLocators.REGISTER_PASSWORD2)
+        input_registre_password2.send_keys("sonfgys19")
+        button_registre = self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON)
+        button_registre.click()
+
+    def register_user(self, email, password):
+        email_field = self.browser.find_element(*LoginPageLocators.EMAIL_TO_LOGIN)
+        email_field.send_keys(email)
+        password_field = self.browser.find_element(*LoginPageLocators.PASSWORD_TO_LOGIN)
+        password_field.send_keys(password)
+        button_to_login = self.browser.find_element(*LoginPageLocators.BUTTON_TO_LOGIN)
+        button_to_login.click()
+
+    def register_new_user(self, email, password):
+        email_field = self.browser.find_element(*LoginPageLocators.REGISTER_EMAIL)
+        email_field.send_keys(email)
+        password1_field = self.browser.find_element(*LoginPageLocators.REGISTER_PASSWORD1)
+        password1_field.send_keys(password)
+        password2_field = self.browser.find_element(*LoginPageLocators.REGISTER_PASSWORD2)
+        password2_field.send_keys(password)
+        button_to_register = self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON)
+        button_to_register.click()        
+    
+
+
+        
+        
+        
+        
+        
         
