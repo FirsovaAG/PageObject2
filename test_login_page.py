13a6d2b775c28dab1290283700da917a891f328f
FirsovaAG <61489394+FirsovaAG@users.noreply.github.com>
2020-04-14 20:44:05 +0300

    added check  test_regster_new_user

--- test_login_page.py
+++ test_login_page.py
@@ -1,5 +1,6 @@
 from .pages.login_page import LoginPage
 import time
+import pytest
 
 def test_guest_should_see_login_form(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
@@ -7,3 +8,13 @@ def test_guest_should_see_login_form(browser):
     page.open()                      # открываем страницу
     time.sleep(10)
     page.should_be_login_page()
+    
+
+def test_regster_new_user(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.register_for_guest()
+    time.sleep(10)
+    page.should_be_authorized_user()
+    
